eb9f248ae264b72fbee885545e8e80dcfa2d1ed0
Muhammad Mudassir <mmudassirarain04@gmail.com>
2025-06-11 17:05:38 +0500

    Update App.js: use HashRouter for GitHub Pages routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import './index.css';
 
 import Navbar from './Navbar';
 import Home from './Home';
-import About from './About'
+import About from './About';
 import Program from './Program';
 import Donate from './Donate';
 import Events from './Events';
@@ -16,17 +16,17 @@ function App() {
   return (
     <Router>
       <Navbar />
-        <div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/program" element={<Program />} />
-            <Route path="/donate" element={<Donate />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-          <Footer />
-        </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/program" element={<Program />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
